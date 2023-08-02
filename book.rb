a6c7e9415a15f78d8e327e09c0ab6002f58e7886
toyman640 <falakoadebayor@gmail.com>
2023-08-02 18:57:30 +0100

    Fix linters error

--- book.rb
+++ book.rb
@@ -1,14 +1,13 @@
 # A class representing a book with certain attributes and methods.
 class Book
-  attr_accessor :title
-  attr_accessor :author
-  attr_accessor :rental
+  attr_accessor :title, :author, :rental
+
   def initialize(title, author)
     @title = title
     @author = author
     @rental = []
   end
-  
+
   def add_rental(rental)
     @rental << rental
   end
--- classroom.rb
+++ classroom.rb
@@ -6,6 +6,4 @@ class Classroom
   end
 
   attr_accessor :label
-
-
 end
