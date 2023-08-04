adb2ffb1e8756d0ba633b47d3a7e2ec74dcd60c9
toyman640 <falakoadebayor@gmail.com>
2023-08-04 14:35:40 +0100

    Fix linters error

--- app.rb
+++ app.rb
@@ -29,6 +29,40 @@ class App
     puts(@people.map { |person| "[#{person.type}] Name: #{person.name}, ID: #{person.id} Age: #{person.age}" })
   end
 
+  # def create_person
+  #   puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+  #   choice = gets.chomp.to_i
+  #   puts 'Age:'
+  #   age = gets.chomp.to_i
+  #   puts 'Name:'
+  #   name = gets.chomp
+  #   parent_permission = false
+  #   if choice == 1
+  #     loop do
+  #       puts 'Has parent permission? [Y/N]:'
+  #       parent_permission_input = gets.chomp.downcase
+  #       parent_permission = parent_permission_input == 'y'
+  #       break if %w[y n].include?(parent_permission_input)
+  #     end
+  #     classroom = if parent_permission
+  #                   (puts('Enter classroom label:')
+  #                    Classroom.new(gets.chomp))
+  #                 end
+  #     person = Student.new(age, classroom, name: name, parent_permission: parent_permission)
+  #     person.type = 'Student'
+  #   elsif choice == 2
+  #     puts 'Enter Specialization:'
+  #     specialization = gets.chomp
+  #     person = Teacher.new(age, specialization, name: name, parent_permission: true)
+  #     person.type = 'Teacher'
+  #   else
+  #     puts 'Invalid choice. Please enter 1 for student or 2 for teacher.'
+  #     return
+  #   end
+  #   @people << person
+  #   puts 'Person created successfully'
+  # end
+
   def create_person
     puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
     choice = gets.chomp.to_i
@@ -36,31 +70,54 @@ class App
     age = gets.chomp.to_i
     puts 'Name:'
     name = gets.chomp
-    parent_permission = false
+
     if choice == 1
-      loop do
-        puts 'Has parent permission? [Y/N]:'
-        parent_permission_input = gets.chomp.downcase
-        parent_permission = parent_permission_input == 'y'
-        break if %w[y n].include?(parent_permission_input)
-      end
-      classroom = if parent_permission
-                    (puts('Enter classroom label:')
-                     Classroom.new(gets.chomp))
-                  end
-      person = Student.new(age, classroom, name: name, parent_permission: parent_permission)
-      person.type = 'Student'
+      # parent_permission = parent_permission_get
+      # classroom = parent_permission ? classroom_get : nil
+      # person = create_student(age, name, parent_permission, classroom)
+      person = create_student(age, name, parent_permission_get, parent_permission_get ? classroom_get : nil)
     elsif choice == 2
-      puts 'Enter Specialization:'
-      specialization = gets.chomp
-      person = Teacher.new(age, specialization, name: name, parent_permission: true)
-      person.type = 'Teacher'
+      person = create_teacher(age, name)
     else
       puts 'Invalid choice. Please enter 1 for student or 2 for teacher.'
       return
     end
-    @people << person
-    puts 'Person created successfully'
+
+    if person
+      @people << person
+      puts 'Person created successfully'
+    else
+      puts 'Invalid choice. Please enter 1 for student or 2 for teacher.'
+    end
+  end
+
+  def create_student(age, name, parent_permission, classroom)
+    student = Student.new(age, classroom, name: name, parent_permission: parent_permission)
+    student.type = 'Student'
+    student
+  end
+
+  def create_teacher(age, name)
+    puts 'Enter Specialization:'
+    specialization = gets.chomp
+    teacher = Teacher.new(specialization, age, name: name, parent_permission: true)
+    teacher.type = 'Teacher'
+    teacher
+  end
+
+  def parent_permission_get
+    loop do
+      puts 'Has parent permission? [Y/N]:'
+      parent_permission_input = gets.chomp.downcase
+      return parent_permission_input == 'y' if %w[y n].include?(parent_permission_input)
+
+      puts "Invalid input. Please enter 'Y' or 'N' only."
+    end
+  end
+
+  def classroom_get
+    puts 'Enter classroom label:'
+    Classroom.new(gets.chomp)
   end
 
   def create_book
